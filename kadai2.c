a3165b1ea5a16a73a5d94a1548947d494a770d15
t236059 <t236059@st.yamagata-u.ac.jp>
2024-11-01 19:20:29 +0900

    Add files via upload

---
+++ kadai2.c
@@ -0,0 +1,18 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("double型の最大値:%g\n", DBL_MAX);
+    printf("double型の最小値:%g\n", DBL_MIN);
+    printf("double型の精度:%d\n", DBL_DIG);
+    printf("int型の最大値:%d\n", INT_MAX);
+    printf("int型の最値小値:%d\n", INT_MIN);
+    
+    printf("char型の符号なし最大値:%g\n", UCHAR_MAX);
+    printf("char型の最大値:%g\n", CHAR_MAX);
+    printf("char型の最小値:%g\n", CHAR_MIN);
+    
+    return 0;
+}
